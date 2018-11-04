d326ef1c16d1e3fef9848755c3d0b807f8a33e87
Gary Webb <gary@thecloudandcompass.com>
2018-11-04 13:34:44 +0000

    Refactored Rover movement Integration Tests in preparation for more tests to be added

--- PlutoRoverKata/PlutoRoverKataTests/IntegrationTests/RoverTests.cs
+++ PlutoRoverKata/PlutoRoverKataTests/IntegrationTests/RoverTests.cs
@@ -1,5 +1,6 @@
 ﻿using PlutoRoverKata;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -15,15 +16,19 @@ namespace PlutoRoverKataTests.IntegrationTests
             _rover = new Rover(new Navigator());
         }
 
-        [Fact]
-        public void Given_the_rover_is_at_0_0_north_When_moving_forward_once_Then_it_should_be_at_0_1_north()
+        [Theory]
+        [ClassData(typeof(RoverMovementTestData))]
+        public void When_a_command_is_sent_from_NASA_Then_the_rover_is_moved_to_the_expected_location(
+            int startX, int startY, Heading startHeading, 
+            string command, 
+            int expectedEndX, int expectedEndY, Heading expectedEndHeading)
         {
-            SetLocation(_rover, 0, 0, Heading.North);
-            _rover.Move("F");
+            SetLocation(_rover, startX, startY, startHeading);
+            _rover.Move(command);
 
-            Assert.True(_rover.Location.X == 0);
-            Assert.True(_rover.Location.Y == 1);
-            Assert.True(_rover.Location.Heading == Heading.North);
+            Assert.True(_rover.Location.X == expectedEndX);
+            Assert.True(_rover.Location.Y == expectedEndY);
+            Assert.True(_rover.Location.Heading == expectedEndHeading);
         }
 
         private void SetLocation(Rover rover, int x, int y, Heading heading)
@@ -32,5 +37,15 @@ namespace PlutoRoverKataTests.IntegrationTests
             rover.Location.Y = y;
             rover.Location.Heading = heading;
         }
+    }
+
+    public class RoverMovementTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { 0, 0, Heading.North, "F", 0, 1, Heading.North };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
